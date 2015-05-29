64e878e20e098c41011679a383f52bdf23c6ab5d
Matthew Pierce <mrapierce@gmail.com>
2015-05-29 14:27:41 -0700

    Add status validations and tests

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -22,8 +22,15 @@ class StatusesControllerTest < ActionController::TestCase
 		get :new
 		assert_response :success
 	end
+	
+	test "should be logged in to post a status" do
+		post :create, status: { content: "Hello" }
+		assert_response :redirect
+		assert_redirected_to new_user_session_path
+	end
 
-  test "should create status" do
+  test "should create status when logged in" do
+		sign_in users(:matthew)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
@@ -36,15 +43,31 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+		sign_in users(:matthew)
+		
     get :edit, id: @status
     assert_response :success
   end
+	
+	test "should be logged in to edit a status" do
+		get :edit, id: @status
+		assert_response :redirect
+		assert_redirected_to new_user_session_path
+	end
 
-  test "should update status" do
+  test "should update status when logged in" do
+		sign_in users(:matthew)
+		
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
+	
+	test "should be logged in to update a status" do
+		patch :update, id: @status, status: { content: @status.content }
+		assert_response :redirect
+		assert_redirected_to new_user_session_path
+	end
 
   test "should destroy status" do
     assert_difference('Status.count', -1) do
